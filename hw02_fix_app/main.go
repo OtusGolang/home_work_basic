5cc0c71384f4b417574a659b357294836dad2601
Aleksei Semushkin <aleksei.semushkin@semrush.com>
2023-07-22 16:15:54 +0400

    hw -1

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -8,13 +8,18 @@ import (
 )
 
 func init() {
-	var path string
+	var path string = "data.json"
 
 	fmt.Printf("Enter data file path: ")
 	fmt.Scanln(&path)
 
 	var err error
 	var staff []types.Employee
+
+	if len(path) == 0 {
+		path = "data.json"
+	} else {
+	}
 
 	staff, err = reader.ReadJSON(path, -1)
 
--- hw02_fix_app/reader/pkg.go
+++ hw02_fix_app/reader/pkg.go
@@ -1,19 +1,22 @@
 package reader
 
-import "github.com/fixme_my_friend/hw02_fix_app/types"
 import	"encoding/json"
 import	"fmt"
 import	"io"
 import	"os"
 
+import	"github.com/fixme_my_friend/hw02_fix_app/types"
+
+
 func ReadJSON(filePath string, limit int) ([]types.Employee, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error: %v", err)
 	}
 
 	byte, err := io.ReadAll(f)
 	if err != nil {
+		fmt.Printf("Error: %v", err)
 		return nil, nil
 	}
 

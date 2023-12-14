6df560cf87d0b2dbcd0aa0947a5b1b41ee94900a
Tikhon Kharnavtcov <t.kharnavtsov@energo.app>
2023-12-15 00:36:22 +0300

    dz

--- hw02_fix_app/go.mod
+++ hw02_fix_app/go.mod
@@ -1,5 +1,3 @@
 module github.com/fixme_my_friend/hw02_fix_app
 
 go 1.20
-
-require golang.org/x/example v0.0.0-20230714141244-83a29069fa80 // indirect
--- hw02_fix_app/types/employee.go
+++ hw02_fix_app/types/employee.go
@@ -3,12 +3,13 @@ package types
 import "fmt"
 
 type Employee struct {
-	UserID       int    `json:"user_id"`
+	UserID       int    `json:"userId"`
 	Age          int    `json:"age"`
 	Name         string `json:"name"`
-	DepartmentID int    `json:"department_id"`
+	DepartmentID int    `json:"departmentId"`
+	///FirstName    string `json:"firstName"`
 }
 
 func (e Employee) String() string {
-	return fmt.Sprintf("User ID: %d; Age: %d; Name: %s; Department ID: %d; ", e.UserID, e.Age, e.FirstName, e.DepartmentID)
+	return fmt.Sprintf("UserId: %d; Age: %d; Name: %s; DepartmentId: %d;", e.UserID, e.Age, e.Name, e.DepartmentID)
 }

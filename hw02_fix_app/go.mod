09d1cf3ce8961da0313f1b7738ecc80340c82884
Vasiliy K <vasiliy.kotikov@auxo.team>
2023-12-17 17:36:14 +0300

    HW02 Fix App

--- hw02_fix_app/go.mod
+++ hw02_fix_app/go.mod
@@ -1,5 +1,18 @@
-module github.com/fixme_my_friend/hw02_fix_app
+module github.com/aasdhajkshd/home_work_basic/tree/master/hw02_fix_app
 
 go 1.20
 
-require golang.org/x/example v0.0.0-20230714141244-83a29069fa80 // indirect
+require (
+	github.com/daixiang0/gci v0.12.0 // indirect
+	github.com/fossasia/gci18.fossasia.org v0.0.0-20190122145421-23c953488dc8 // indirect
+	github.com/hexops/gotextdiff v1.0.3 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/spf13/cobra v1.6.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
+	golang.org/x/example v0.0.0-20231013143937-1d6d2400d402 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -1,14 +1,15 @@
-package init
+package main
 
 import (
-	"github.com/fixme_my_friend/hw02_fix_app/printer"
-	"github.com/fixme_my_friend/hw02_fix_app/reader"
-	"github.com/fixme_my_friend/hw02_fix_app/types"
 	"fmt"
+
+	"github.com/aasdhajkshd/home_work_basic/tree/master/hw02_fix_app/printer"
+	"github.com/aasdhajkshd/home_work_basic/tree/master/hw02_fix_app/reader"
+	"github.com/aasdhajkshd/home_work_basic/tree/master/hw02_fix_app/types"
 )
 
-func init() {
-	var path string = "data.json"
+func main() {
+	path := "data.json"
 
 	fmt.Printf("Enter data file path: ")
 	fmt.Scanln(&path)
@@ -19,11 +20,14 @@ func init() {
 	if len(path) == 0 {
 		path = "data.json"
 	} else {
+		fmt.Println("Successfully read data.json")
 	}
 
-	staff, err = reader.ReadJSON(path, -1)
+	staff, err = reader.ReadJSON(path)
 
-	fmt.Print(err)
+	if err != nil {
+		fmt.Print(err)
+	}
 
 	printer.PrintStaff(staff)
 }

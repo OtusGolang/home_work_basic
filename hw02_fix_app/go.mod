c8f49ed92bdf0d595fb95e94963d4426073862ee
Роман Крупов <romankrupov@MacBook-Air-Roman.local>
2024-02-26 18:04:34 +0300

    hw02_fix_app

--- hw02_fix_app/go.mod
+++ hw02_fix_app/go.mod
@@ -1,8 +1,3 @@
-module github.com/Krupovroman/home_work_basic/hw02_fix_app
+module github.com/krupovroman/home_work_basic/hw02_fix_app
 
 go 1.20
-
-require (
-	github.com/golang/protobuf v1.5.3 // indirect
-	google.golang.org/protobuf v1.32.0 // indirect
-)
--- hw02_fix_app/types/employee.go
+++ hw02_fix_app/types/employee.go
@@ -6,10 +6,9 @@ type Employee struct {
 	UserID       int    `json:"user_id"`
 	Age          int    `json:"age"`
 	Name         string `json:"name"`
-	FirstName    string `json:"first_name"`
 	DepartmentID int    `json:"department_id"`
 }
 
 func (e Employee) String() string {
-	return fmt.Sprintf("User ID: %d; Age: %d; Name: %s; Department ID: %d; ", e.UserID, e.Age, e.FirstName, e.DepartmentID)
+	return fmt.Sprintf("User ID: %d; Age: %d; Name: %s; Department ID: %d; ", e.UserID, e.Age, e.Name, e.DepartmentID)
 }
